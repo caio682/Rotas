c518718b015fe1d32becc0c9fd8c57ce270a0c04
caio682 <79763392+caio682@users.noreply.github.com>
2023-06-14 21:52:56 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { IndexModule } from './pages/index/index.module';
+import { PortifolioModule } from './pages/portifolio/portifolio.module';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
+const routes: Routes = [
+  { path: '', component: TitleComponent, pathMatch: 'full' },
+  {path: 'portifolio', component: CardComponent, pathMatch: 'prefix'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
